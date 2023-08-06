d8b61b5c8ceb95678205350d90e969de70c87f22
zhengjie <jerryxuzhengjie@gmail.com>
2023-08-06 13:58:59 +0800

    [Enhance] fix bugs

--- model.py
+++ model.py
@@ -10,6 +10,11 @@ import xgboost as xgb
 
 from sklearn.metrics import r2_score
 from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import Sequential
 from tqdm import tqdm
 
 
@@ -101,9 +106,12 @@ def predict():
     # 加载模型文件
     model = xgb.Booster(model_file='model/xgb.model')
     # 预测
-    data = pd.read_excel('data/test.xlsx',index_col=0)
+    data = pd.read_excel('data/test.xlsx', index_col=0)
     # 调整列顺序MDEC-23,MDEO-12,maxssO,minHBa,VCH-5,maxdssC,VC-5,mindssC,minHsOH,ETA_Shape_Y,maxHsOH,minHBint5,minsssN,maxHBint8,LipoaffinityIndex,BCUTc-1h,ATSc3,ETA_BetaP_s,ETA_EtaP_F,mindsCH
-    data = data.loc[:,['MDEC-23', 'MDEO-12', 'maxssO', 'minHBa', 'VCH-5', 'maxdssC', 'VC-5', 'mindssC', 'minHsOH', 'ETA_Shape_Y', 'maxHsOH', 'minHBint5', 'minsssN', 'maxHBint8', 'LipoaffinityIndex', 'BCUTc-1h', 'ATSc3', 'ETA_BetaP_s', 'ETA_EtaP_F', 'mindsCH']]
+    data = data.loc[:,
+           ['MDEC-23', 'MDEO-12', 'maxssO', 'minHBa', 'VCH-5', 'maxdssC', 'VC-5', 'mindssC', 'minHsOH', 'ETA_Shape_Y',
+            'maxHsOH', 'minHBint5', 'minsssN', 'maxHBint8', 'LipoaffinityIndex', 'BCUTc-1h', 'ATSc3', 'ETA_BetaP_s',
+            'ETA_EtaP_F', 'mindsCH']]
     print(data)
     dtest = xgb.DMatrix(data)
     y_pred = model.predict(dtest)
@@ -137,134 +145,123 @@ def normal_xgb_train(X_train, X_test, y_train, y_test):
 
 
 def svr_train(X_train, X_test, y_train, y_test):
-    # 定义搜索空间
-    bounds = [(-5, 5), (-5, 5), (-5, 5), (0, 10)]
-
-    # 定义适应度函数
-    def fitness_function(params):
-        C, gamma, coef0, degree = params
-        clf = SVR(kernel='rbf', C=10 ** C, gamma=10 ** gamma, coef0=10 ** coef0, degree=int(degree))
-        clf.fit(X_train, y_train)
-        rmse = np.sqrt(mean_squared_error(y_test, clf.predict(X_test)))
-        return -rmse
-
-    fitness_function([0, 0, 0, 0])
-
-    # 初始化种群
-    pop_size = 20
-    population = []
-    for i in range(pop_size):
-        individual = []
-        for bound in bounds:
-            individual.append(np.random.uniform(low=bound[0], high=bound[1]))
-        population.append(individual)
-
-    # 初始mse和r2
-    C, gamma, coef0, degree = individual[0], individual[1], individual[2], individual[3]
-    clf = SVR(kernel='rbf', C=10 ** C, gamma=10 ** gamma, coef0=10 ** coef0, degree=int(degree))
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    # print(y_pred)
-
+    # 实例化一个SVR模型
+    svr = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    # 用训练数据训练模型
+    svr.fit(X_train, y_train)
+    # 预测测试数据
+    y_pred = svr.predict(X_test)
     # 评估预测结果
-    # rmse
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print("初始RMSE: %f" % (rmse))
+    train_rmse = np.sqrt(mean_squared_error(y_train, svr.predict(X_train)))
+    print("SVR 训练集RMSE: %f" % train_rmse)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print("SVR 测试集RMSE: %f" % test_rmse)
+    # R2
+    print("SVR 训练集R2: %f" % r2_score(y_train, svr.predict(X_train)))
+    print("SVR 测试集R2: %f" % r2_score(y_test, y_pred))
+    # MAE
+    print("SVR 训练集MAE: %f" % mean_absolute_error(y_train, svr.predict(X_train)))
+    print("SVR 测试集MAE: %f" % mean_absolute_error(y_test, y_pred))
 
-    # 迭代优化
-    max_gen = 200
-    for gen in tqdm(range(max_gen)):
-        # 计算适应度
-        fitness = [fitness_function(individual) for individual in population]
 
-        # 找到最优解
-        best_idx = np.argmin(fitness)
-        best_solution = population[best_idx]
-        best_fitness = fitness[best_idx]
-
-        # 移动和聚集
-        for i in range(pop_size):
-            if i != best_idx:
-                # print(best_solution)
-                # print(population[i])
-                dx = []
-                for j in range(len(bounds)):
-                    dx.append(best_solution[j] - population[i][j])
-                # dx = best_solution - population[i]
-                # print(np.dot(np.random.uniform(), dx))
-                # 添加柯西变异
-                cauchy = np.random.standard_cauchy(len(bounds))
-                # 添加高斯变异
-                gaussian = np.random.normal(0, 1, len(bounds))
-                v = (population[i] + np.dot(np.random.uniform(), dx)) * (
-                        1 + (1 - gen ** 2 / max_gen ** 2) * cauchy + gen ** 2 / max_gen ** 2 * gaussian)
-                # 边界处理
-                # print(v)
-                # print(bounds[0])
-                v = np.clip(v, [b[0] for b in bounds], [b[1] for b in bounds])
-                # 计算适应度
-                fitness_v = fitness_function(v)
-                # 更新个体
-                if fitness_v < fitness[i]:
-                    population[i] = v
-                    fitness[i] = fitness_v
-
-        # 生成新的个体
-        for i in range(pop_size):
-            idx1, idx2, idx3 = np.random.choice(pop_size, size=3, replace=False)
-            r1, r2, r3 = population[idx1], population[idx2], population[idx3]
-            r1 = np.array(r1)
-            r2 = np.array(r2)
-            r3 = np.array(r3)
-            # 添加柯西变异
-            cauchy = np.random.standard_cauchy(len(bounds))
-            # 添加高斯变异
-            gaussian = np.random.normal(0, 1, len(bounds))
-            v = (r1 + 0.5 * (r2 - r3)) * (
-                    1 + (1 - gen ** 2 / max_gen ** 2) * cauchy + gen ** 2 / max_gen ** 2 * gaussian)
-            # 边界处理
-            v = np.clip(v, [b[0] for b in bounds], [b[1] for b in bounds])
-            # 计算适应度
-            fitness_v = fitness_function(v)
-            # 更新个体
-            if fitness_v < fitness[i]:
-                population[i] = v
-                fitness[i] = fitness_v
-        # if (gen + 1) % 10 == 0:
-        print("Iteration: {}, Best Fitness: {:.6f}".format(gen, best_fitness))
-
-    # 输出最优解
-    print("Best solution:", best_solution)
-    print("Best fitness:", best_fitness)
-
-    # 预测结果
-    C, gamma, coef0, degree = best_solution
-    clf = SVR(kernel='rbf', C=10 ** C, gamma=10 ** gamma, coef0=10 ** coef0, degree=int(degree))
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-    print(y_pred)
-
+def gbdt_train(X_train, X_test, y_train, y_test):
+    # GBDT
+    # 建立模型
+    model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+    # 训练模型
+    model.fit(X_train, y_train)
+    # 预测
+    y_pred = model.predict(X_test)
     # 评估预测结果
-    # rmse
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print("优化后的RMSE: %f" % (rmse))
+    train_rmse = np.sqrt(mean_squared_error(y_train, model.predict(X_train)))
+    print("GBDT 训练集RMSE: %f" % train_rmse)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print("GBDT 测试集RMSE: %f" % test_rmse)
+    # R2
+    print("GBDT 训练集R2: %f" % r2_score(y_train, model.predict(X_train)))
+    print("GBDT 测试集R2: %f" % r2_score(y_test, y_pred))
+    # MAE
+    print("GBDT 训练集MAE: %f" % mean_absolute_error(y_train, model.predict(X_train)))
+    print("GBDT 测试集MAE: %f" % mean_absolute_error(y_test, y_pred))
+
+
+class CNN_LSTM(nn.Module):
+    def __init__(self):
+        super(CNN_LSTM, self).__init__()
+
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=64, kernel_size=1)
+        self.pool1 = nn.MaxPool1d(2)
+        self.lstm = nn.LSTM(input_size=64, hidden_size=50, num_layers=1, batch_first=True)
+        self.fc = nn.Linear(50, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x, _ = self.lstm(x.transpose(1, 2))
+        x = self.fc(x[:, -1, :])
+        return x
+
+
+def cnn_lstm_train(X_train, X_test, y_train, y_test):
+    # CNN-LSTM
+    # 重构数据
+    X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
+    X_test = X_test.values.reshape((X_test.shape[0], 1, X_test.shape[1]))
+    y_train = y_train.values.reshape((y_train.shape[0], 1))
+    y_test = y_test.values.reshape((y_test.shape[0], 1))
+    # 转换成torch的tensor
+    X_train = torch.from_numpy(X_train).float()
+    X_test = torch.from_numpy(X_test).float()
+    y_train = torch.from_numpy(y_train).float()
+    y_test = torch.from_numpy(y_test).float()
+    # 打印数据形状
+    print(X_train.shape, y_train.shape)
+    # 建立模型
+    model = CNN_LSTM()
+    # 定义损失函数
+    criterion = nn.MSELoss()
+    # 定义优化器
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    # 训练模型
+    for epoch in tqdm(range(3000)):
+        optimizer.zero_grad()
+        output = model(X_train)
+        loss = criterion(output, y_train)
+        loss.backward()
+        optimizer.step()
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+
+    # 使用训练好的模型进行预测
+    with torch.no_grad():
+        x_pred = model(X_train)
+        y_pred = model(X_test)
+        # 评估预测结果
+        train_rmse = np.sqrt(mean_squared_error(y_train, x_pred))
+        print("CNN_LSTM 训练集RMSE: %f" % train_rmse)
+        test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        print("CNN_LSTM 测试集RMSE: %f" % test_rmse)
+        # R2
+        print("CNN_LSTM 训练集R2: %f" % r2_score(y_train, x_pred))
+        print("CNN_LSTM 测试集R2: %f" % r2_score(y_test, y_pred))
+        # MAE
+        print("CNN_LSTM 训练集MAE: %f" % mean_absolute_error(y_train, x_pred))
+        print("CNN_LSTM 测试集MAE: %f" % mean_absolute_error(y_test, y_pred))
 
 
 def main():
-    # data = data_loader()
-    # X = data.iloc[:, 2:]
-    # Y = data.loc[:, ['pIC50']]
-    #
-    # # X筛除掉BCUTc-1l,SPC=6,ShsOH,minsOH,maxHBint5这五列
-    # X = X.drop(['BCUTc-1l', 'SPC-6', 'SHsOH', 'minsOH', 'maxHBint5'], axis=1)
-    #
-    # # 划分数据集
-    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=123)
-    # # normal_xgb_train(X_train, X_test, y_train, y_test)
+    data = data_loader()
+    X = data.iloc[:, 2:]
+    Y = data.loc[:, ['pIC50']]
+    X = X.drop(['minHsOH', 'VC-5', 'SsOH', 'MLogP', 'maxsOH'], axis=1)
+
+    # 划分数据集
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=123)
+    # normal_xgb_train(X_train, X_test, y_train, y_test)
     # bayesian_optimized_xgb_train(X_train, X_test, y_train, y_test)
-    # # svr_train(X_train, X_test, y_train, y_test)
-    predict()
+    # svr_train(X_train, X_test, y_train, y_test)
+    # gbdt_train(X_train, X_test, y_train, y_test)
+    cnn_lstm_train(X_train, X_test, y_train, y_test)
+    # predict()
 
 
 if __name__ == '__main__':
